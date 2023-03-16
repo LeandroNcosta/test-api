55495a8e215b59c7bbb197096da0fa5547df6089
Leandro Costa <lcosta200212@gmail.com>
2023-03-16 18:05:30 -0300

    update

--- src/server.js
+++ src/server.js
@@ -5,5 +5,6 @@ dotenv.config()
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`)
+  // console.log(`Servidor rodando em http://localhost:${port}`)
+  console.log(`Servidor rodando em na porta ${port}`)
 })
